bc3ae655feb0cc80a558414bb0356571b6d26e3b
bilmuherdogans <ierd>
2022-10-16 21:12:07 +0300

    Car user test class was update.

--- DesignPatterns/src/com/creationalpatterns/factorypattern/abstractFactory/OtomobileExamples/CarUserTest.java
+++ DesignPatterns/src/com/creationalpatterns/factorypattern/abstractFactory/OtomobileExamples/CarUserTest.java
@@ -4,12 +4,24 @@ public class CarUserTest
 {
 	public static void main(String[] args) 
 	{
-		CarFactoryImpl carFactory=new CarFactoryImpl();
-		PassengersCar golf=carFactory.createPassengerCarFactory("12345","Golf");
-		CommercialCar transporter=carFactory.createCommercialCarFactory("12345", "Transporter", "1500");
+		System.err.println("\n****** Sup level\n");
+		CarFactory carFactory=new CarFactoryImpl();
+		Car golf=carFactory.createPassengerCarFactory("12345","Golf");
+		Car transporter=carFactory.createCommercialCarFactory("12345", "Transporter", "1500");
 		
 		System.err.println(golf.toString());
 		System.err.println(transporter.toString());
+		
+		System.err.println("\n****** Sub level\n");
+		
+		CarFactoryImpl carFactoryImpl=new CarFactoryImpl();
+		PassengersCar tofas=carFactoryImpl.createPassengerCarFactory("11111","Tofas");
+		CommercialCar iveco=carFactoryImpl.createCommercialCarFactory("22222", "Iveco", "12500");
+		
+		System.err.println(tofas.toString());
+		System.err.println(iveco.toString());
+		System.err.println("\n******\n");
+	
 	}
 
 }
